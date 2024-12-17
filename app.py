3610f3ddee25d57741d0bfa5fa181d7a9e469e7f
W.A.H.U.W Arachchi <145230618+hiranwasala@users.noreply.github.com>
2024-12-17 09:31:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', debug=False)
